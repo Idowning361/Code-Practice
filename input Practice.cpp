2cdbac637e694877cce681f86e70c2b2bdd28bdb
Idowning361 <150697892+Idowning361@users.noreply.github.com>
2024-09-23 12:57:52 -0500

    Add files via upload

---
+++ input Practice.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string myName;
+    cout<<"Hello What's Your Name?"<<endl;
+        cin>>myName;
+    cout<<"Hello "<<myName<<endl;
+    return 0;
+}
